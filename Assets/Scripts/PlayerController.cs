9b9267ec92b919b121d791a03f58f200bc6c839b
FabianOrtiz1110 <112835551+FabianOrtiz1110@users.noreply.github.com>
2023-11-09 04:30:49 -0800

    LessonC
    LessonC

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,6 +8,9 @@ public class PlayerController : MonoBehaviour
     GameObject focalPoint;
     Renderer rendererPlayer;
     public float speed = 10.0f;
+    public float PowerUpSpeed = 10.0f;
+    bool hasPowerUp = false;
+    public GameObject powerUpInd;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,6 +22,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        powerUpInd.transform.position = transform.position;
         float forwardInput = Input.GetAxis("Vertical");
         float magnitude = forwardInput * speed * Time.deltaTime;
         rbPlayer.AddForce(focalPoint.transform.forward * magnitude, ForceMode.Force);
@@ -34,5 +38,35 @@ public class PlayerController : MonoBehaviour
             {
                 rendererPlayer.material.color = new Color(1.0f + forwardInput, 1.0f, 1.0f + forwardInput);
             }
+
+    }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if(other.CompareTag("PowerUp"))
+            {
+                hasPowerUp = true;
+                Destroy(other.gameObject);
+                StartCoroutine(PowerUpCountdown());
+                powerUpInd.SetActive(true);
+            }
+        }
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            if(hasPowerUp && collision.gameObject.CompareTag("Enemy"))
+            {
+                Debug.Log("Player has collided with" + collision.gameObject +" with powerup set to: " +hasPowerUp);
+                Rigidbody rbEnemy = collision.gameObject.GetComponent<Rigidbody>();
+                Vector3 awayDir = collision.gameObject.transform.position = transform.position;
+
+                rbEnemy.AddForce(awayDir * PowerUpSpeed, ForceMode.Impulse);
+            }
+        }
+        IEnumerator PowerUpCountdown()
+        {
+            yield return new WaitForSeconds(8);
+            hasPowerUp = false; 
+            powerUpInd.SetActive(false);
         }
     }
